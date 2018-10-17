ccd8014d896c04342634cd87bc733bcac68930b5
Elronnd <elronnd@elronnd.net>
2018-10-16 20:47:33 -0700

    STATIC CONTENT IS RUNNING

--- src/web.lisp
+++ src/web.lisp
@@ -12,11 +12,11 @@
 
 (setf (ningle:route *app* "/")
       (lambda (params)
-        (html (list '(head (link :type "text/css" :rel "stylesheet" :href "stylesheet.css")) (cons 'body *header*)))))
+        (html (list '(head (link :type "text/css" :rel "stylesheet" :href "/static/stylesheet.css")) (cons 'body *header*)))))
         ;'(head (link :type "text/css" :rel "stylesheet" :href "stylesheet.css"))
 
 (defun web-quit ()
   (clack:stop *handler*))
 (defun web-main ()
   (defparameter *handler*
-    (clack:clackup *app* :port *port* :server :woo :silent nil :use-thread t)))
+    (clack:clackup *app* :port *port* :server :fcgi :silent nil :use-thread t)))
